4f8dba71a9a4cd2e9977200224942e062d908647
Joice Doll <joice.doll2@yahoo.com>
2023-11-19 22:44:43 -0300

    feat:api implementation init

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,17 +1,27 @@
 import http from "http";
-
+import findTicket from "./api/ticket/findTicket";
+const url = require('url');
 const server = http
   .createServer(function (req, res) {
     console.log("Server is running");
 
     res.setHeader("Content-Type", "application/json");
-    switch (req.url) {
-      case "/books":
-        res.writeHead(200);
-        res.end("teste");
+    switch (req.method) {
+      case 'GET':
+        handleGetRequest(path, findTicket.ticket, res);
         break;
     }
   })
   .listen(process.env.LISTEN);
+
+  function handleGetRequest(path, query, res) {
+    if (path === '/books') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('GET: List of books');
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+    }
+  }
 
 export default server;
