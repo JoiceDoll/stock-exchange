95e6ddd553bac47f4685317535f26a0873636e8d
Joice Doll <joice.doll2@yahoo.com>
2023-11-23 21:55:46 -0300

    feat: data api created

--- Server/src/api/ticket/controllers/findTickerController.ts
+++ Server/src/api/ticket/controllers/findTickerController.ts
@@ -7,8 +7,16 @@ class findTicket {
     await axios
       .get(process.env.BASE_URL + `search=${ticket}`)
       .then(function (response) {
-        const result = response.data;
-        return res.status(200).json(result);
+        const [result] = response.data.stocks;
+
+        console.log(result.name);
+        const data = {
+          name: result.name,
+          close: result.close,
+          logo: result.logo,
+        };
+
+        return res.status(200).json(data);
       })
       .catch(function (error) {
         return res.status(400).json({ message: "Ticket error", error });
