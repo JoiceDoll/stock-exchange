6b3b5289b28486935f53ce04f097866964998f78
Joice Doll <joice.doll2@yahoo.com>
2023-12-06 21:52:40 -0300

    feat: cors config added

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,9 +1,9 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import routers from "./routes/routes"
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(routers);
 
