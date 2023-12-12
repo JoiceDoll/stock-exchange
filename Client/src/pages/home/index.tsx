326afa550cb09080320a1fefc02f62b9175874f4
Joice Doll <joice.doll2@yahoo.com>
2023-12-11 22:57:26 -0300

    feat:screen inited

--- Client/src/pages/home/index.tsx
+++ Client/src/pages/home/index.tsx
@@ -9,13 +9,14 @@ function Home() {
           <p className="p-5 text-xl text-white">Account</p>
         </div>
 
-        <div className="w-[60%] h-[45%] flex justify-around absolute right-0 top-40 cursor-pointer ">
+        <div className="w-[60%] h-[45%] flex justify-around absolute right-0 top-40 cursor-pointer">
           <Link to="/companySummary" className="w-[30%]">
             <div className="w-[100%] h-[100%] bg-custom-card rounded-xl flex justify-center hover:bg-custom-hover-green">
               <div className="flex flex-col justify-center align-middle w-[100%] gap-3">
                 <img
                   src={EnterpriseIcon}
-                  className="w-[20%] h-[20%] mx-[40%]" alt="Enterprise Icon"
+                  className="w-[20%] h-[20%] mx-[40%]"
+                  alt="Enterprise Icon"
                 />
                 <p className="text-center w-[100%]  text-white font-bold">
                   Resumo da empresa
@@ -26,7 +27,11 @@ function Home() {
           <Link to="/financialData " className="w-[30%]">
             <div className="w-[100%] h-[100%] bg-custom-card rounded-xl flex justify-center cursor-pointer hover:bg-custom-hover-green">
               <div className="flex flex-col justify-center align-middle w-[100%] gap-3">
-                <img src={EuroIcon} className="w-[20%] h-[20%] mx-[40%]" alt="Financial Icon"/>
+                <img
+                  src={EuroIcon}
+                  className="w-[20%] h-[20%] mx-[40%]"
+                  alt="Financial Icon"
+                />
                 <p className="text-center w-[100%]  text-white font-bold">
                   Dados financeiros
                 </p>
