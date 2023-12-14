3bb3e94f95be73f1de536473f13bf9aadce79207
Joice Doll <joice.doll2@yahoo.com>
2023-12-13 21:46:18 -0300

    fix: indentation

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,12 +1,10 @@
 import express from "express";
-import cors from "cors"
-import routers from "./routes/routes"
+import cors from "cors";
+import routers from "./routes/routes";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routers);
-
-
 
 export default app;
